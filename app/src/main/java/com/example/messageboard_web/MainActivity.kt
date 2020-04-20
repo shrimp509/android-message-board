e5f7ff3b328f9866ef3b37b9a4224de83fb11f9e
RongSon <shrimp509@hotmail.com>
2020-04-20 21:31:41 +0800

    Initial commit

---
+++ app/src/main/java/com/example/messageboard_web/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.messageboard_web
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webview = findViewById<WebView>(R.id.main_webview)
+        webview.settings.javaScriptEnabled = true
+        webview.webViewClient = WebViewClient()
+        webview.loadUrl("http://yun-ru-tseng.gitlab.io/messageboard/#/messageBoard/")
+    }
+}
